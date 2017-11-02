5e70eafedc2696beb6a7e857a3ab6eea315c1bb6
AmrataRamchandani <amrata.2016@gmail.com>
2017-11-03 00:40:45 +0530

    pushing the code with mess and extra commented code,might be needed later

--- heartbeatex4/server.js
+++ heartbeatex4/server.js
@@ -1,7 +1,8 @@
-var app = require('express')();
+var express = require('express');
+var app=express();
 var session = require('express-session');
 var http = require('http').Server(app);
-//var io = require('socket.io')(http);
+var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,6 +12,8 @@ app.use(session({
   saveUninitialized: true
 }));
 
+
+
 app.get('/admin', function (req, res) {
 res.sendFile(__dirname + '/admin.html');
 });
@@ -19,43 +22,95 @@ app.get('/connectedsocketids', function(req, res) {
 res.send(connectedSockets);
 })
 
+
+app.get('/username', function(req, res) {
+res.send(req.session.username);
+})
+
+
 app.get('/',function(req,res){
 	// Check if roomid is set in the session.
 	// If it is, we will redirect to the attempt/roomid page.
-	if(req.session.roomid) {
-	    res.redirect('/attempt/'+req.session.roomid);
+	if(req.session.username) {
+	    res.redirect('/home');
 	}
 	else {
-	   res.sendFile(__dirname + '/attempt.html');
+	   res.sendFile(__dirname + '/login.html');
 	}
 });
 
-app.post('/attempt', function (req, res) {
-  var roomid=req.body.username+req.body.quizid;
-  req.session.quizid = req.body.quizid;
+app.post('/login', function (req, res) {
+  // var roomid=req.body.username+req.body.quizid;
+  // req.session.quizid = req.body.quizid;
 	req.session.username = req.body.username;
-  req.session.roomid=roomid;
-  res.redirect('/attempt/'+roomid);
+  res.redirect('/home');
+//  res.sendFile(__dirname + '/a.html');
+
+
   res.end();
 });
 
-app.get('/attempt/:roomid', function(req,res){
+app.get('/home', function (req, res) {
+  if(req.session.username) {
+      res.write('<h1>Welcome ' + req.session.username+' !</h1>' )
+      var quiz1roomidlink=("/attempt/"+req.session.username+'1').split(' ').join('');
+      var quiz2roomidlink=("/attempt/"+req.session.username+'2').split(' ').join('');;
+      var quiz3roomidlink=("/attempt/"+req.session.username+'3').split(' ').join('');;
 
-  if(req.session.roomid) {
-  res.write('<h1>Welcome ' + req.session.username+' !</h1>' );
-  res.write('<h2>You have choosed to attempt quiz : ' + req.session.quizid + '</h2>');
-  res.write('<a href="/finish">Finish Attempt</a>');
-  res.end();
+      res.write('<h3><a href='+quiz1roomidlink+' >Quiz 1</a></h3>');
+      res.write('<h3><a href='+quiz2roomidlink+' >Quiz 2</a></h3>');
+      res.write('<h3><a href='+quiz3roomidlink+' >Quiz 3</a></h3>');
+      res.write('<a href="/logout">Logout</a>');
+      res.end();
   }else{
     res.write('<h1>Please Login first.</h1>');
     res.write('<a href="/">Login</a>');
     res.end();
   }
+});
+
+
+// app.get('/attemptquiz', function(req,res){
+//   if(req.session.username) {
+//     res.sendFile(__dirname + '/attempt.html');
+// }else{
+//   res.write('<h1>Please Login first.</h1>');
+//   res.write('<a href="/">Login</a>');
+//   res.end();
+// }
+
+// app.post('/attempt', function(req,res){
+//   req.session.quizid = req.body.quizid;
+//   var roomid=req.session.username+req.body.quizid;
+//   req.session.roomid=roomid;
+//   res.redirect('/attempt/'+roomid);
+// });
+
+
+  // });
+app.get('/attempt/:roomid', function(req,res){
+res.sendFile(__dirname + '/attempt.html');
+
+  // if(req.session.roomid) {
+//  res.write('<h1>Welcome ' + req.session.username+' !</h1>' );
+//  res.write('<h2>You have choosed to attempt quiz : ' + req.session.quizid + '</h2>');
+//res.redirect('/attempt/'+roomid);
+//  res.write('<a href="/finish">Finish Attempt</a>');
+//  res.end();
+  // }else{
+  //   res.write('<h1>Please Login first.</h1>');
+  //   res.write('<a href="/">Login</a>');
+  //   res.end();
+  // }
 
 });
 
 app.get('/finish',function(req,res){
+  res.redirect('/home');
+});
 
+
+app.get('/logout',function(req,res){
 	// if the user logs out, destroy all of their individual session
 	// information
 	req.session.destroy(function(err) {
@@ -68,32 +123,39 @@ app.get('/finish',function(req,res){
 
 });
 
+var connectedSockets =[];
+io.on('connection', function(socket){
+  console.log(socket.id+' Socket id connected');
 
-// var connectedSockets =[];
-// io.on('connection', function(socket){
-//   console.log(socket.id+' Socket id connected');
-//   connectedSockets.push(socket.id);
-//
-//   socket.on('disconnect', function(){
-//     console.log(socket.id+' Socket id disconnected');
-//     connectedSockets.splice(connectedSockets.indexOf(socket.id), 1);
-//   });
-//
-//   // socket.on('to server', function(msg){
-// 	// 	 io.emit('to client', msg);
-//   // });
-//
-//   socket.on('to server quizid', function(quizid){
-//   //   io.emit('to client', msg);
-//   });
-//
-//   socket.on('to server username', function(username){
-//   //   io.emit('to client', msg);
-//   });
-//
-// });
+  socket.on('attempt', function(data){
+    // socket.username=data.username;
+  //   socket.quizid=data.quizid;
+  //   console.log(data);
+  var socketobject = {username:data.username,quizid:data.quizid,socketid:socket.id};
+  connectedSockets.push(socketobject);
+    });
 
 
+//  connectedSockets.push(socket.id);
+
+  socket.on('disconnect', function(){
+    console.log(socket.id+' Socket id disconnected');
+    deletedSockets.push(socket.id);
+  //  connectedSockets.splice(connectedSockets.indexOf(socket.id), 1);
+  // find the index of the object having socket id which left.
+  var index = connectedSockets.findIndex(function(o){
+      return o.id === socket.id;
+  });
+  // Remove the socket object from the connected array.
+  if (index !== -1) connectedSockets.splice(index, 1);
+  });
+
+});
+var deletedSockets =[];
+// Send deletedsockets array to the admin page.
+app.get('/deletedsocketids', function(req, res) {
+res.send(deletedSockets);
+})
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
