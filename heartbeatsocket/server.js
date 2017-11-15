7ae3e6a745ac327d91a5639bda36730ce9969769
AmrataRamchandani <amrata.2016@gmail.com>
2017-11-15 18:45:00 +0530

    Change the livestatus algo with livetable in DB

--- heartbeatsocket/server.js
+++ heartbeatsocket/server.js
@@ -64,9 +64,64 @@ io.on('connection', function(socket){
                  +socket.statusConnected+","+socket.ip+","+socket.timestampC+")";
     con.query(sql, function (err, result) {
       if (err) throw err;
-     console.log('Record for Connected Socket ID : '+socket.socketid+' is inserted into DB');
+  //   console.log('Record for Connected Socket ID : '+socket.socketid+' is inserted into DB');
     });
-});
+
+    var liveordead = "select username,quizid,roomid,"+
+                "count(case when socketstatus = 'Connected' then socketid end) as count_connected_socketid, "+
+                "count(case when socketstatus = 'Disconnected' then socketid end) as count_disconnected_socketid "+
+              "from socketinfo where roomid="+socket.roomid+
+              "group by username,quizid,roomid";
+    con.query(liveordead, function (err, result) {
+        if (err) throw err;
+         for(i in result){
+           countC=result[i].count_connected_socketid;
+           countD=result[i].count_disconnected_socketid;
+         }
+
+         if(countC > countD)
+         {
+           socket.livestatus="'Live'";
+           socket.livetime=0;
+           socket.deadtime=0;
+           socket.maxdisconnecttime=0;
+         var liverecordexistsql ="Select * from livetable where roomid="+socket.roomid;
+         con.query(liverecordexistsql, function (err, result){
+           if (err) throw err;
+           if(result.length > 0 ){
+
+             for(i in result){
+               status=result[i].status;
+               maxdisconnecttime=result[i].maxdisconnecttime;
+              var deadtime=result[i].deadtime;
+              console.log(socket.roomid+': Previous deadtime: '+humanise(deadtime));
+             }
+
+             if(status=='Dead'){
+               console.log(socket.roomid+': Current Deadtime is :'+humanise(socket.timestampC-maxdisconnecttime));
+                deadtime=parseInt(deadtime)+parseInt(socket.timestampC-maxdisconnecttime);
+                 console.log(socket.roomid+': After cumulation,deadtime is : '+humanise(deadtime));
+
+               var updatelivetablesql = "Update livetable set status="+socket.livestatus+",deadtime="+deadtime+
+               ",minconnecttime="+socket.timestampC+" where roomid="+socket.roomid;
+               con.query(updatelivetablesql, function (err, result){
+                    if (err) throw err;
+                });
+             }
+
+           }
+           else{
+             var livetablesql = "INSERT INTO livetable (roomid,status,minconnecttime,maxdisconnecttime,livetime,deadtime) VALUES" +
+                        "("+socket.roomid+","+socket.livestatus+","+socket.timestampC+",0,0,0 )";
+             con.query(livetablesql, function (err, result){
+                  if (err) throw err;
+              });
+           }
+         });
+       }
+        });
+  });
+
 
   socket.on('disconnect', function(){
        socket.timestampD=new Date().getTime();
@@ -87,11 +142,50 @@ io.on('connection', function(socket){
 
       con.query(sql, function (err, result) {
         if (err) throw err;
-        console.log('Record for Disconnected Socket ID : '+socket.socketid+' is inserted into DB');
+        //console.log('Record for Disconnected Socket ID : '+socket.socketid+' is inserted into DB');
     });
-});
-});
 
+    var liveordead = "select username,quizid,roomid,"+
+                "count(case when socketstatus = 'Connected' then socketid end) as count_connected_socketid, "+
+                "count(case when socketstatus = 'Disconnected' then socketid end) as count_disconnected_socketid "+
+              "from socketinfo where roomid="+socket.roomid+
+              "group by username,quizid,roomid";
+    con.query(liveordead, function (err, result) {
+        if (err) throw err;
+         for(i in result){
+           countC=result[i].count_connected_socketid;
+           countD=result[i].count_disconnected_socketid;
+         }
+
+         if(countC == countD)
+         {
+           socket.livestatus="'Dead'";
+           var fetchmintimesql="select minconnecttime,livetime from livetable where roomid="+socket.roomid;
+           con.query(fetchmintimesql, function (err, result) {
+               if (err) throw err;
+                for(i in result){
+                  var minconnecttime=result[i].minconnecttime;
+                  var livetime=result[i].livetime;
+                  console.log(socket.roomid+':Previous livetime: '+humanise(livetime));
+                }
+                //  here socket.timestampD is the maxdisconnecttime;
+
+                console.log(socket.roomid+':Current livetime is :'+humanise(socket.timestampD-minconnecttime));
+                    livetime=parseInt(livetime)+parseInt(socket.timestampD-minconnecttime);
+                    console.log(socket.roomid+':After cumulation,livetime  is :'+humanise(livetime));
+
+                    var updatelivetablesql = "Update livetable set status="+socket.livestatus+", maxdisconnecttime= "+
+                   socket.timestampD+",livetime="+livetime+"  where roomid="+socket.roomid;
+
+                    con.query(updatelivetablesql, function (err, result){
+                         if (err) throw err;
+                     });
+              });
+         }
+        });
+
+});
+});
 
 app.get('/livestatus', function(req, res) {
 var sql = "select username,quizid,roomid, "+
@@ -103,9 +197,19 @@ var sql = "select username,quizid,roomid, "+
           "group by username,quizid,roomid";
   con.query(sql, function (err, result,fields) {
     if (err) throw err;
+
   res.send(result);
   });
 });
+
+function humanise(difference){
+  var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  var time = days+' days, '+hours+' hrs, '+minutes+' mins, '+seconds+' secs' ;
+  return time;
+}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
